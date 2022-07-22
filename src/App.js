ef359b25ce22428f7a558664a68695c030939f9b
Kane95 <jhpark95@greenit.cc>
2022-07-22 16:43:01 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../src/component/Header";
+import CreatWord from "./component/CreatWord";
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import EmptyRage from "./component/EmptyRage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreatWord />} />
+          <Route path="*" element={<EmptyRage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/component/CreatWord.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreatWord() {
+  return <div>CreatWord</div>;
+}
